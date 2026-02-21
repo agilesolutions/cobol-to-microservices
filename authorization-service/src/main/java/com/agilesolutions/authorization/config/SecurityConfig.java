e20c1fd20bb0c1ab74d70b908c9a32afa0ade484
admin <rong@agile-solutions.ch>
2026-02-21 16:18:28 +0100

    add authorization service

--- authorization-service/src/main/java/com/agilesolutions/authorization/config/SecurityConfig.java
+++ authorization-service/src/main/java/com/agilesolutions/authorization/config/SecurityConfig.java
@@ -2,16 +2,21 @@ package com.agilesolutions.authorization.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.time.Duration;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -19,16 +24,43 @@ import java.util.UUID;
 public class SecurityConfig {
 
     @Bean
-    public RegisteredClient registeredClient() {
-        return RegisteredClient.withId(UUID.randomUUID().toString())
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
+                new OAuth2AuthorizationServerConfigurer();
+
+        http
+                .securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
+                .with(authorizationServerConfigurer, (authorizationServer) ->
+                        authorizationServer
+                                .tokenEndpoint(tokenEndpoint ->
+                                        tokenEndpoint
+                                                .accessTokenRequestConverter(
+                                                        new ClientCredentialsOnlyAuthenticationConverter()
+                                                )
+                                )
+                )
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClient() {
+
+        RegisteredClient client = RegisteredClient.withId(UUID.randomUUID().toString())
                 .clientId("gateway-client-id")
-                .clientSecret("gateway-client-secret")
+                .clientSecret("{noop}gateway-client-secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .scopes(s-> s.addAll(List.of("read", "write")))
+                .scope("read")
+                .scope("write")
                 .tokenSettings(TokenSettings.builder()
                         .accessTokenTimeToLive(Duration.ofMinutes(30))
                         .build())
                 .build();
+
+        return new InMemoryRegisteredClientRepository(client);
     }
 
     @Bean
